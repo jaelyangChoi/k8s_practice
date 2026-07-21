d2de1f107b25c5ca8bd21136f31dce96da4559e2
JaeryangChoi <jaelyangchoi@gmail.com>
2026-07-21 23:15:56 +0900

    fix:msa

--- 3.msa/ordering/src/main/java/com/example/ordersystem/ordering/service/ProductFeign.java
+++ 3.msa/ordering/src/main/java/com/example/ordersystem/ordering/service/ProductFeign.java
@@ -9,8 +9,7 @@ import org.springframework.web.bind.annotation.*;
 //실제 http 호출이면서, 마치 로컬 메서드인 것처럼 간단하게 사용.
 //name은 eureka에 등록된 호출할 서비스의 이름
 //url은 k8s에서 service명
-//@FeignClient(name = "product-service") //, url="http://product-service") //프로파일로 분리
-@FeignClient(url="http://product-service")
+@FeignClient(name = "product-service", url = "http://product-service") //프로파일로 분리할 것
 public interface ProductFeign {
 
     @GetMapping("/product/{productId}")
